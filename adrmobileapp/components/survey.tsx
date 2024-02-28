cb952c97ec8e493e73d758ed257d32313d828840
Jeffery Wei <jefferywei.5765ho7@gmail.com>
2024-02-28 06:04:31 -0500

    added storing to database

--- adrmobileapp/components/survey.tsx
+++ adrmobileapp/components/survey.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-import { View, Text, useColorScheme } from 'react-native';
+import { View, Text, useColorScheme, Button } from 'react-native';
 import { RadioButton } from 'react-native-paper';
+import {initializeFirebase} from '../config/firebase';
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, doc, setDoc } from "firebase/firestore"; 
 
-export const Survey = (props: {numOfOptions: number; question: string; options: string[];}) => {
+initializeFirebase();
+const firestore = getFirestore();
+
+export const Survey = (props: {question: string; options: string[]; onResponse: (response: string) => void;}) => {
     const [selectedValue, setSelectedValue] = useState(props.options[0]);
     const theme = useColorScheme();
     const isDarkTheme = theme === 'dark';
 
+    // Getter method to get the selected value
+    const getResponse = () => selectedValue;
+    
     return (
         <View>
             <Text style={[isDarkTheme ? { color: 'white' } : { color: 'black' }]}>
@@ -14,10 +23,13 @@ export const Survey = (props: {numOfOptions: number; question: string; options: 
             </Text>
 
             <RadioButton.Group
-                onValueChange={(value) => setSelectedValue(value)}
+                onValueChange={(value) => {
+                    setSelectedValue(value);
+                    props.onResponse(value);
+                }}
                 value={selectedValue}
             >
-                {props.options.slice(0, props.numOfOptions).map((option, index) => (
+                {props.options.map((option, index) => (
                     <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
                         <RadioButton value={option}/>
                         <Text style={[isDarkTheme ? { color: 'white' } : { color: 'black' }]}>
@@ -26,10 +38,6 @@ export const Survey = (props: {numOfOptions: number; question: string; options: 
                     </View>
                 ))}
             </RadioButton.Group>
-
-            {/* <Text style={[isDarkTheme ? { color: 'white' } : { color: 'black' }]}>
-                Selected Value: {selectedValue}
-            </Text> */}
         </View>
     );
   };
--- adrmobileapp/pages/presurveys.tsx
+++ adrmobileapp/pages/presurveys.tsx
@@ -1,8 +1,66 @@
 import React, { useState } from 'react';
-import { View, Text, useColorScheme } from 'react-native';
+import { View, Text, useColorScheme, Button } from 'react-native';
 import {Survey} from '../components/survey';
+import {initializeFirebase} from '../config/firebase';
+import { getFirestore } from "firebase/firestore";
+import { collection, serverTimestamp, doc, setDoc } from "firebase/firestore"; 
   
 export function PreSurvey(): React.JSX.Element {
+    const questionOptions = [
+        {
+            question: 'How often does our family read together?',
+            options: ["Daily", "Weekly", "Monthly", "We don't read together"]
+        },
+        {
+            question: 'How often does my child read for pleasure?',
+            options: ["Daily", "Weekly", "Monthly", "Not at all"]
+        },
+        {
+            question: 'My child enjoys reading books for pleasure.',
+            options: ["A great deal", "Some", "A little", "Not at all"]
+        },
+        {
+            question: 'How important is it that my child enjoys reading?',
+            options: ["A great deal", "Some", "A little", "Not at all"]
+        },
+        {
+            question: 'Do you believe a program that encourages families reading together would affect my child’s reading enjoyment?',
+            options: ["Yes", "No", "Not sure"]
+        }
+    ];
+
+    // State to store responses
+    const initialResponses = questionOptions.map((question) => question.options[0]);
+    const questions = questionOptions.map((question) => question.question);
+    const [responses, setResponses] = useState<string[]>(initialResponses);
+    
+    // Function to handle survey response
+    const handleResponse = (index: number, response: string) => {
+        const newResponses = [...responses];
+        newResponses[index] = response;
+        setResponses(newResponses);
+    };
+
+    initializeFirebase();
+    const firestore = getFirestore();
+
+    function writeSurvey(){
+        const surveyDB = doc(collection(firestore, "surveys"));
+        const questionData = {
+            type: 'PreProgram',
+            questions: questions,
+        };
+        setDoc(surveyDB, questionData);
+        const responsesCollection = doc(collection(surveyDB, "responses"));
+
+        const responseData = {
+            answers: responses,
+            submittedAt: serverTimestamp(),
+        };
+
+        setDoc(responsesCollection, responseData);
+    }
+
     const theme = useColorScheme();
     const isDarkTheme = theme === 'dark';
 
@@ -11,36 +69,18 @@ export function PreSurvey(): React.JSX.Element {
             <Text style={[isDarkTheme ? { color: 'white', fontWeight: 'bold' } : { color: 'black', fontWeight: 'bold' }]}>
                 Pre-Survey
             </Text>
+            {questionOptions.map((question, index) => (
+                <Survey
+                    key={index}
+                    question={question.question}
+                    options={question.options}
+                    onResponse={(response) => handleResponse(index, response)}
+                />
+            ))}
 
-            <Text style={{fontWeight: 'bold'}}>Pre-survey</Text>
-            <Survey 
-            question = "How often does our family read together?" 
-            numOfOptions = {4}
-            options = {["Daily", "Weekly", "Monthly", "We don't read together"]}
-            />
-
-            <Survey 
-            question = "How often does my child read for pleasure?"
-            numOfOptions = {4}
-            options = {["Daily", "Weekly", "Monthly", "Not at all"]} 
-            />
-
-            <Survey 
-            question = "My child enjoys reading books for pleasure:" 
-            numOfOptions = {4}
-            options = {["A great deal", "Some", "A little", "Not at all"]} 
-            />
-
-            <Survey 
-            question = "How important is it that my child enjoys reading?" 
-            numOfOptions = {4}
-            options = {["A great deal", "Some", "A little", "Not at all"]} 
-            />
-
-            <Survey 
-            question = "Do you believe a program that encourages families reading together would affect my child’s reading enjoyment?" 
-            numOfOptions = {3}
-            options = {["Yes", "No", "Not sure"]}
+            <Button
+                title="Submit"
+                onPress={writeSurvey}
             />
         </View>
     );
